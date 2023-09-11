aeada41cdf9fbb5512de855545eea19147d45a02
croumy <clemence@playmoweb.com>
2023-09-11 09:57:37 +0200

    add ci to create release and build wearOS apk

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0-rc01" apply false
+    id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.46" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
@@ -9,5 +9,6 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+        classpath("com.android.tools.build:gradle:8.1.1")
     }
 }
