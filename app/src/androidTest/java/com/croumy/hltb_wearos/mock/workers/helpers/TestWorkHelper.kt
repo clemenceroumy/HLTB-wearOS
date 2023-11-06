b20807dddff7a20c40877da47235e8cb04527aec
Clémence Roumy <clemenceroumy@gmail.com>
2023-11-06 14:41:41 +0100

    fix: remove SaveTimeWorker binding to TestWorkHelper to use generic class

--- app/src/androidTest/java/com/croumy/hltb_wearos/mock/workers/helpers/TestWorkHelper.kt
+++ app/src/androidTest/java/com/croumy/hltb_wearos/mock/workers/helpers/TestWorkHelper.kt
@@ -1,10 +1,11 @@
 package com.croumy.hltb_wearos.mock.workers
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Data
 import androidx.work.ListenableWorker
+import androidx.work.await
 import androidx.work.testing.TestListenableWorkerBuilder
-import com.croumy.hltb_wearos.presentation.workers.SaveTimeWorker
 import com.croumy.hltb_wearos.presentation.workers.interfaces.IWorkerHelper
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
@@ -13,12 +14,14 @@ import javax.inject.Singleton
 @Singleton
 class TestWorkHelper @Inject constructor(val workerFactory: SaveTimeWorkerFactory): IWorkerHelper {
     override fun launchWorker(workerClass: Class<out ListenableWorker>, data: Data?, context: Context, name: String) {
-        val uploadWorkRequest = TestListenableWorkerBuilder<SaveTimeWorker>(context)
+        Log.i("TestWorkHelper", workerClass.name)
+
+        val uploadWorkRequest = TestListenableWorkerBuilder.from(context, workerClass)
             .setWorkerFactory(workerFactory)
             .build()
 
         runBlocking {
-            uploadWorkRequest.doWork()
+            uploadWorkRequest.startWork().await()
         }
     }
 }
